aa71c48534f55962a0def9a48ed17f1f36da9472
tmcarvalho <taniammc.94@gmail.com>
2022-04-09 20:15:49 +0100

    transform to function

--- kanon.py
+++ kanon.py
@@ -5,19 +5,25 @@ Such a variable is similar to a binary target variable, where 1 is a singleout
 and 0 otherwise.
 """
 
-# %%
 from os import sep, getcwd
 import pandas as pd
 import numpy as np
 
-# %%
-data = pd.read_csv(f'{getcwd()}{sep}dataset.csv')
-# %%
-keyVars = ['age', 'det_ind_code', 'vet_benefits', 'year']
-# %%
-data_copy = data.copy()
-# %%
-k = data.groupby(keyVars)[keyVars[0]].transform(len)
-# %%
-data_copy['single_out'] = None
-data_copy['single_out'] = np.where(k == 1, 1, 0)
+def single_outs():
+    """Creates a new binary variable with singleouts information based on selected quasi-identifiers
+
+    Returns:
+        dataframe: Dataframe with single outs variable
+    """
+    data = pd.read_csv(f'{getcwd()}{sep}dataset.csv')
+
+    key_vars = ['age', 'det_ind_code', 'det_occ_code', 'weeks_worked', 'year']
+
+    data_copy = data.copy()
+
+    k = data.groupby(key_vars)[key_vars[0]].transform(len)
+
+    data_copy['single_out'] = None
+    data_copy['single_out'] = np.where(k == 1, 1, 0)
+
+    return data_copy, key_vars

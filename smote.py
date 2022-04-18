77be5b388dfbca6ce800742d134977f6da73ac71
tmcarvalho <taniammc.94@gmail.com>
2022-04-18 17:53:25 +0100

    add predictive performance

--- smote.py
+++ smote.py
@@ -8,13 +8,14 @@ from imblearn.over_sampling import SMOTE
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from collections import ChainMap
 from kanon import single_outs
-from record_linkage import *
+from record_linkage import record_linkage
+from modeling import evaluate_model
 
 # %%
 data = pd.read_csv(f'{getcwd()}{sep}dataset.csv')
 data, key_vars = single_outs(data)
-# %%
 # %% apply LabelEncoder beacause of smote
 data = data.apply(LabelEncoder().fit_transform)
 # %% create list of possiblilities to interpolation
@@ -22,6 +23,8 @@ n = pd.Series(np.arange(2*data['single_out'].value_counts()[1],
                 int(data['single_out'].value_counts()[0]/2), 1))
 # %%
 knn = [1, 2, 3, 4, 5]
+validation = {}
+test = {}
 for nn in knn:
     smote = SMOTE(random_state=42,
                 k_neighbors=nn,
@@ -37,14 +40,32 @@ for nn in knn:
     oversample = x_smote.copy()
     oversample = oversample.drop(data[data['single_out']==1].index).reset_index(drop=True)
     # save oversampled data
-    oversample.to_csv(f'{getcwd()}{sep}output{sep}oversampled_data{sep}oversample_knn{nn}.csv', index=False)
-
-    # apply record linkage
+    oversample.to_csv(f'{getcwd()}{sep}output{sep}oversampled_data{sep}oversample_knn{nn}.csv',
+        index=False)
+    
+    # apply record linkage only to single outs!!
     oversample_singleouts = oversample[oversample['single_out']==1]
     original_singleouts = data[data['single_out']==1]
     potential_matches = record_linkage(oversample_singleouts, original_singleouts, key_vars)
     # save record linkage results
-    potential_matches.to_csv(f'{getcwd()}{sep}output{sep}record_linkage{sep}potential_matches_knn{nn}.csv', index=False)
+    potential_matches.to_csv(
+        f'{getcwd()}{sep}output{sep}record_linkage{sep}potential_matches_knn{nn}.csv',
+        index=False)
 
-    # remove single_out var to prediction
+    # prepare data to modeling
+    X, y = data.iloc[:, :-2], data.iloc[:, -2]
+    # predictive performance
+    validation_result, test_result = evaluate_model(X, y, validation, test, nn)
+    print(len(validation_result))
+    validation = ChainMap(validation,validation_result)
+    print(len(validation))
+    test = ChainMap(test,test_result)
+
 # %%
+# save validation and test results
+np.save(
+    f'{getcwd()}{sep}output{sep}modeling{sep}oversampled{sep}validation{sep}validation.npy',
+    validation)
+np.save(
+    f'{getcwd()}{sep}output{sep}modeling{sep}oversampled{sep}test{sep}test.npy',
+    test)

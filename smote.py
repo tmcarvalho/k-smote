26cb7723047351545a1151c998763943f8505c93
tmcarvalho <taniammc.94@gmail.com>
2022-04-22 20:35:59 +0100

    apply record linkage

--- smote.py
+++ smote.py
@@ -3,69 +3,100 @@ This script will apply SMOTE technique in the single out cases.
 """
 # %%
 from os import sep, getcwd
-import random
 from imblearn.over_sampling import SMOTE
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
-from collections import ChainMap
 from kanon import single_outs
 from record_linkage import record_linkage
 from modeling import evaluate_model
 
 # %%
 data = pd.read_csv(f'{getcwd()}{sep}dataset.csv')
-data, key_vars = single_outs(data)
-# %% apply LabelEncoder beacause of smote
-data = data.apply(LabelEncoder().fit_transform)
-# %% create list of possiblilities to interpolation
-n = pd.Series(np.arange(2*data['single_out'].value_counts()[1],
-                int(data['single_out'].value_counts()[0]/2), 1))
-# %%
-knn = [1, 2, 3, 4, 5]
-validation = {}
-test = {}
-for nn in knn:
-    smote = SMOTE(random_state=42,
-                k_neighbors=nn,
-                sampling_strategy={1: random.choice(n)})
-    # fit predictor and target variable
-    X = data[data.columns[:-1]]
-    y = data.iloc[:, -1]
-    x_smote, y_smote = smote.fit_resample(X, y)
+set_data, set_key_vars = single_outs(data)
 
-    # add single out to apply record linkage
-    x_smote['single_out'] = y_smote
-    # remove original single outs from oversample
-    oversample = x_smote.copy()
-    oversample = oversample.drop(data[data['single_out']==1].index).reset_index(drop=True)
-    # save oversampled data
-    oversample.to_csv(f'{getcwd()}{sep}output{sep}oversampled_data{sep}oversample_knn{nn}.csv',
-        index=False)
-    
-    # apply record linkage only to single outs!!
-    oversample_singleouts = oversample[oversample['single_out']==1]
-    original_singleouts = data[data['single_out']==1]
-    potential_matches = record_linkage(oversample_singleouts, original_singleouts, key_vars)
+# %%
+def apply_record_linkage(oversample_data, original_data, keys, args):
+    """Apply record linkage and calculate the percentage of re-identification
+
+    Args:
+        oversample_data (pd.Dataframe): oversampled data
+        original_data (pd.Dataframe): original dataframe
+        keys (list): list of quasi-identifiers
+        args (list): list with 3 values:
+        nr of iteration of QIs; nr of knn; percentage in smote
+
+    Returns:
+        _type_: _description_
+    """
+    oversample_singleouts = oversample_data[oversample_data['single_out']==1]
+    original_singleouts = original_data[original_data['single_out']==1]
+    potential_matches = record_linkage(oversample_singleouts, original_singleouts, keys)
     # save record linkage results
     potential_matches.to_csv(
-        f'{getcwd()}{sep}output{sep}record_linkage{sep}potential_matches_knn{nn}.csv',
-        index=False)
+        f'{getcwd()}{sep}output{sep}record_linkage{sep}potential_matches_\
+            QI{args[0]}_knn{args[1]}_per{args[2]}.csv', index=False)
 
-    # prepare data to modeling
-    X, y = data.iloc[:, :-2], data.iloc[:, -2]
-    # predictive performance
-    validation_result, test_result = evaluate_model(X, y, validation, test, nn)
-    print(len(validation_result))
-    validation = ChainMap(validation,validation_result)
-    print(len(validation))
-    test = ChainMap(test,test_result)
+    # get acceptable score (QIs match at least 50%)
+    acceptable_score = potential_matches[potential_matches['Score'] >= \
+        0.5*potential_matches['Score'].max()]
+    level_1_acceptable_score = acceptable_score.groupby(['level_1'])['level_0'].size()
+    per = ((1/level_1_acceptable_score.min()) * 100) / len(oversample_data)
 
+    # get max score (all QIs match)
+    max_score = potential_matches[potential_matches['Score'] == len(keys)]
+    # find original single outs with an unique match in oversampled data - 100% match
+    level_1_max_score = max_score.groupby(['level_1'])['level_0'].size()
+    per_100 = (len(level_1_max_score[level_1_max_score == 1]) * 100) / len(oversample_data)
+
+    return per, per_100
 # %%
-# save validation and test results
-np.save(
-    f'{getcwd()}{sep}output{sep}modeling{sep}oversampled{sep}validation{sep}validation.npy',
-    validation)
-np.save(
-    f'{getcwd()}{sep}output{sep}modeling{sep}oversampled{sep}test{sep}test.npy',
-    test)
+# key_vars_idx = [data.columns.get_loc(c) for c in key_vars if c in data]
+knn = [1, 2, 3, 4, 5]
+percentages = [0.1, 0.2, 0.3, 0.5, 0.7]
+
+for idx, key_vars in enumerate(set_key_vars):
+    # apply LabelEncoder beacause of smote
+    dt = set_data[idx].apply(LabelEncoder().fit_transform)
+    for nn in knn:
+        for p in percentages:
+            smote = SMOTE(random_state=42,
+                        k_neighbors=nn,
+                        sampling_strategy=p)
+            # fit predictor and target variable
+            X = dt[dt.columns[:-1]]
+            y = dt.iloc[:, -1]
+            x_smote, y_smote = smote.fit_resample(X, y)
+
+            # add single out to apply record linkage
+            x_smote['single_out'] = y_smote
+            # remove original single outs from oversample
+            oversample = x_smote.copy()
+            oversample = oversample.drop(dt[dt['single_out']==1].index).reset_index(drop=True)
+            # save oversampled data
+            oversample.to_csv(f'{getcwd()}{sep}output{sep}oversampled_data{sep}oversample_QI{idx}_\
+                knn{nn}_per{p}.csv', index=False)
+            percentage, percentage_100 = apply_record_linkage(
+                oversample,
+                dt,
+                key_vars,
+                [idx, nn, p])
+
+            # prepare data to modeling
+            X, y = dt.iloc[:2500, :-2], dt.iloc[:2500, -2]
+            # predictive performance
+            validation, test = evaluate_model(
+                X,
+                y,
+                nn,
+                percentage,
+                percentage_100)
+
+            # save validation and test results
+            np.save(
+                f'{getcwd()}{sep}output{sep}modeling{sep}oversampled{sep}validation{sep}\
+                    validation_QI{idx}_knn{nn}_per{p}.npy', validation)
+            np.save(
+                f'{getcwd()}{sep}output{sep}modeling{sep}oversampled{sep}test{sep}\
+                    test_QI{idx}_knn{nn}_per{p}.npy', test)
+# %%

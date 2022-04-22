270501a7ad234098e8b48685bc7512323aec86ce
tmcarvalho <taniammc.94@gmail.com>
2022-04-22 20:34:20 +0100

    generate 5 sets of QIs

--- kanon.py
+++ kanon.py
@@ -18,13 +18,28 @@ def single_outs(data: pd.DataFrame) -> tuple[pd.DataFrame, list]:
         dataframe: Dataframe with single outs variable
         list: selected quasi-identifiers
     """
+    set_data = []
+    set_key_vars = []
     # select 10% of attributes as quasi-identifiers
     random.seed(42)
-    key_vars = random.choices(data.columns, k=int(0.1*len(data.columns)))
-    k = data.groupby(key_vars)[key_vars[0]].transform(len)
+    for i in range(0, 5):
+        key_vars = random.choices(data.columns[:-1], k=int(0.1*len(data.columns)))
+        set_key_vars.append(key_vars)
 
-    data_copy = data.copy()
-    data_copy['single_out'] = None
-    data_copy['single_out'] = np.where(k == 1, 1, 0)
+        if (i > 0) & len(set_key_vars) != i+1:
+            set_key_vars = [x for x in set_key_vars if x != key_vars]
+            key_vars = random.choices(data.columns, k=int(0.1*len(data.columns)))
+            set_key_vars.append(key_vars)
+            i -= 1
 
-    return data_copy, key_vars
+    for key_vars in set_key_vars:
+        k = data.groupby(key_vars)[key_vars[0]].transform(len)
+
+        data_copy = data.copy()
+        data_copy['single_out'] = None
+        data_copy['single_out'] = np.where(k == 1, 1, 0)
+
+        set_data.append(data_copy)
+
+
+    return set_data, set_key_vars

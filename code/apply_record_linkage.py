8162aee936ee377885c1c44c242b8905b8e83461
tmcarvalho <taniammc.94@gmail.com>
2022-05-29 09:27:33 +0100

    change sdv to record linkage

--- code/apply_record_linkage.py
+++ code/apply_record_linkage.py
@@ -1,112 +1,177 @@
 """_summary_
 """
 # %%
+from io import StringIO
 from os import walk
 import pandas as pd
+import numpy as np
 from record_linkage import threshold_record_linkage
-from kanon import single_outs_sets
-from sdv.metrics.tabular import NumericalLR
 from sklearn.preprocessing import LabelEncoder
 import re
 import gc
 import ast
+import zipfile
+
 # %%
-original_folder = '../original'
-output_rl_folder = '../output/record_linkage/PPT'
-transf_folder = '../PPT'
-_, _, input_files = next(walk(f'{original_folder}'))
-_, _, transf_files = next(walk(f'{transf_folder}'))
+def privacy_risk(transf_file, orig_data, args, list_key_vars):
+    dict_per = {'privacy_risk_50': [], 'privacy_risk_70': [], 'privacy_risk_90':[], 'privacy_risk_100': [], 'ds': []}
+
+    int_transf_files = list(map(int, re.findall(r'\d+', transf_file.split('_')[0])))
+    int_transf_qi = list(map(int, re.findall(r'\d+', transf_file.split('_')[2])))
+    set_key_vars = list_key_vars.loc[list_key_vars['ds']==int_transf_files[0], 'set_key_vars'].values[0]
+    print(transf_file) 
+    print(int_transf_files)
+
+    transf_data = pd.read_csv(f'{args.input_folder}/{transf_file}')
+    if args.type == 'smote_singleouts':
+        transf_data = transf_data[transf_data['single_out']==1]
+    
+    # apply LabelEncoder for modeling
+    orig_data = orig_data.apply(LabelEncoder().fit_transform)
+    transf_data = transf_data.apply(LabelEncoder().fit_transform)
+
+    if int_transf_qi[0] == 0:
+        key_vars = ast.literal_eval(set_key_vars)[0]
+        if args.type == 'smote_singleouts':
+            orig_data = aux_singleouts(key_vars, orig_data)
+        
+        key_vars = [k for k in key_vars if transf_data[k].values[0]!='*']
+        matches, percentages = threshold_record_linkage(
+            transf_data,
+            orig_data,
+            key_vars)
+
+    if int_transf_qi[0] == 1:
+        key_vars = ast.literal_eval(set_key_vars)[1]
+        if args.type == 'smote_singleouts':
+            orig_data = aux_singleouts(key_vars, orig_data)
+
+        key_vars = [k for k in key_vars if transf_data[k].values[0]!='*']
+        matches, percentages = threshold_record_linkage(
+            transf_data,
+            orig_data,
+            key_vars)
+    
+    if int_transf_qi[0] == 2:
+        key_vars = ast.literal_eval(set_key_vars)[2]
+        if args.type == 'smote_singleouts':
+            orig_data = aux_singleouts(key_vars, orig_data)
+
+        key_vars = [k for k in key_vars if transf_data[k].values[0]!='*']
+        matches, percentages = threshold_record_linkage(
+            transf_data,
+            orig_data,
+            key_vars)
+    
+    if int_transf_qi[0] == 3:
+        key_vars = ast.literal_eval(set_key_vars)[3]
+        if args.type == 'smote_singleouts':
+            orig_data = aux_singleouts(key_vars, orig_data)
+
+        key_vars = [k for k in key_vars if transf_data[k].values[0]!='*']
+        matches, percentages = threshold_record_linkage(
+            transf_data,
+            orig_data,
+            key_vars)
+    
+    if int_transf_qi[0] == 4:
+        key_vars = ast.literal_eval(set_key_vars)[4]
+        if args.type == 'smote_singleouts':
+            orig_data = aux_singleouts(key_vars, orig_data)
+
+        key_vars = [k for k in key_vars if transf_data[k].values[0]!='*']
+        matches, percentages = threshold_record_linkage(
+            transf_data,
+            orig_data,
+            key_vars)
+    
+    with zipfile.ZipFile(f'{args.output_folder}/potential_matches.zip', "a", zipfile.ZIP_DEFLATED) as zip_file:
+        s = StringIO()
+        matches.to_csv(s, index=False) 
+        zip_file.writestr(f'{transf_file.split(".csv")[0]}_rl.csv', s.getvalue())
+    # matches.to_csv(f'{args.output_folder}/{transf_file.split(".csv")[0]}_rl.csv', index=False) 
+    dict_per['privacy_risk_50'].append(percentages[0])
+    dict_per['privacy_risk_70'].append(percentages[1])
+    dict_per['privacy_risk_90'].append(percentages[2])
+    dict_per['privacy_risk_100'].append(percentages[3])
+    dict_per['ds'].append(transf_file.split('.csv')[0])
+    gc.collect()
+    
+    return dict_per
 
 
-not_considered_files = [0,1,3,13,23,28,32,36,40,48,54,66,87]
-#dict_per = {'privacy_risk_50': [], 'privacy_risk_75': [], 'privacy_risk_100': [], 'ds': []}
-dict_per = {'ds': [], 'privacy_risk': []}
+def aux_singleouts(key_vars, dt):
+    k = dt.groupby(key_vars)[key_vars[0]].transform(len)
+    dt['single_out'] = None
+    dt['single_out'] = np.where(k == 1, 1, 0)
+    dt = dt[dt['single_out']==1]
+    return dt
 
-list_key_vars = pd.read_csv('../PPT/list_key_vars.csv')
 
-for idx,file in enumerate(input_files):
-    for tf in transf_files:
-        if int(file.split(".csv")[0]) not in not_considered_files:
-            int_transf_files = list(map(int, re.findall(r'\d+', tf.split('_')[0])))
-            int_transf_qi = list(map(int, re.findall(r'\d+', tf.split('_')[2])))
-            set_key_vars = list_key_vars.loc[list_key_vars['ds']==int_transf_files[0], 'set_key_vars'].values[0]
+def privacy_risk_smote_under_over(transf_file, orig_data, args, key_vars, i):
+    dict_per = {'privacy_risk_50': [], 'privacy_risk_70': [], 'privacy_risk_90':[], 'privacy_risk_100': [], 'ds': []}
+    
+    transf_data = pd.read_csv(f'{args.input_folder}/{transf_file}')
+        
+    # apply LabelEncoder for modeling
+    orig_data = orig_data.apply(LabelEncoder().fit_transform)
+    transf_data = transf_data.apply(LabelEncoder().fit_transform)
+    print(transf_file)
 
-            # if (int(file.split(".csv")[0]) in [2,4,5,8,10,14,16,32]) and (int(file.split(".csv")[0]) in int_transf_files):
-            if int(file.split(".csv")[0]) in int_transf_files:
-                orig_data = pd.read_csv(f'{original_folder}/{file}')
-                # apply LabelEncoder to interpolated data (smote)
-                # orig_data = orig_data.apply(LabelEncoder().fit_transform)
-                transf_data = pd.read_csv(f'{transf_folder}/{tf}')
-                # TODO: check index col in all cases before running
+    matches, percentages = threshold_record_linkage(
+        transf_data,
+        orig_data,
+        key_vars)
+    
+    with zipfile.ZipFile(f'{args.output_folder}/potential_matches.zip', "a", zipfile.ZIP_DEFLATED) as zip_file:
+        s = StringIO()
+        matches.to_csv(s, index=False) 
+        zip_file.writestr(f'{transf_file.split(".csv")[0]}_qi{i}_rl.csv', s.getvalue())
+    # matches.to_csv(f'{args.output_folder}/{transf_file.split(".csv")[0]}_qi{i}_rl.csv', index=False) 
+    dict_per['privacy_risk_50'].append(percentages[0])
+    dict_per['privacy_risk_70'].append(percentages[1])
+    dict_per['privacy_risk_90'].append(percentages[2])
+    dict_per['privacy_risk_100'].append(percentages[3])
+    dict_per['ds'].append(transf_file.split('.csv')[0])
+    dict_per['qi'] = i
+    gc.collect()
 
-                if int_transf_qi[0] == 0:
-                    # matches, percentages = threshold_record_linkage(
-                    #     transf_data,
-                    #     orig_data,
-                    #     ast.literal_eval(set_key_vars)[0])
-                    risk = NumericalLR.compute(
-                        orig_data,
-                        transf_data,
-                        key_fields=ast.literal_eval(set_key_vars)[0],
-                        sensitive_fields=orig_data.columns[-1]
-                        )
-                if int_transf_qi[0] == 1:
-                    # matches, percentages = threshold_record_linkage(
-                    #     transf_data,
-                    #     orig_data,
-                    #     ast.literal_eval(set_key_vars)[1])
-                    risk = NumericalLR.compute(
-                        orig_data,
-                        transf_data,
-                        key_fields=ast.literal_eval(set_key_vars)[1],
-                        sensitive_fields=orig_data.columns[-1]
-                        )
-                
-                if int_transf_qi[0] == 2:
-                    # matches, percentages = threshold_record_linkage(
-                    #     transf_data,
-                    #     orig_data,
-                    #     ast.literal_eval(set_key_vars)[2])
-                    risk = NumericalLR.compute(
-                        orig_data,
-                        transf_data,
-                        key_fields=ast.literal_eval(set_key_vars)[2],
-                        sensitive_fields=orig_data.columns[-1]
-                        )
-                
-                if int_transf_qi[0] == 3:
-                    # matches, percentages = threshold_record_linkage(
-                    #     transf_data,
-                    #     orig_data,
-                    #     ast.literal_eval(set_key_vars)[3])
-                    risk = NumericalLR.compute(
-                        orig_data,
-                        transf_data,
-                        key_fields=ast.literal_eval(set_key_vars)[3],
-                        sensitive_fields=orig_data.columns[-1]
-                        )
-                
-                if int_transf_qi[0] == 4:
-                    # matches, percentages = threshold_record_linkage(
-                    #     transf_data,
-                    #     orig_data,
-                    #     ast.literal_eval(set_key_vars)[4])
-                    risk = NumericalLR.compute(
-                        orig_data,
-                        transf_data,
-                        key_fields=ast.literal_eval(set_key_vars)[0],
-                        sensitive_fields=orig_data.columns[-4]
-                        )
+    return dict_per  
 
-                #matches.to_csv(f'{output_rl_folder}/{tf.split(".csv")[0]}_rl.csv', index=False) 
-                #dict_per['privacy_risk_50'].append(percentages[0])
-                #dict_per['privacy_risk_75'].append(percentages[1])
-                #dict_per['privacy_risk_100'].append(percentages[2])
-                dict_per['privacy_risk'].append(risk)
-                dict_per['ds'].append(tf.split('.csv')[0])
-                gc.collect()
-
-risk = pd.DataFrame(dict_per, index=[0])  
-risk.to_csv(f'{output_rl_folder}/total_risk.csv', index=False)
 
 # %% 
+def apply_privacy_risk(transf_file, args):
+    original_folder = 'original'
+    _, _, input_files = next(walk(f'{original_folder}'))
+
+    list_key_vars = pd.read_csv('PPT/list_key_vars.csv')
+    
+    int_transf_files = list(map(int, re.findall(r'\d+', transf_file.split('_')[0])))
+    orig_file = [file for file in input_files if int(file.split(".csv")[0]) == int_transf_files[0]]
+    print(orig_file)
+    orig_data = pd.read_csv(f'{original_folder}/{orig_file[0]}')
+
+    risk = privacy_risk(transf_file, orig_data, args, list_key_vars)
+    total_risk = pd.DataFrame.from_dict(risk)
+    total_risk.to_csv(f'{args.output_folder}/{transf_file.split(".csv")[0]}_per.csv', index=False) 
+
+
+def apply_in_smote_under_over(transf_file, args):
+    original_folder = 'original'
+    _, _, input_files = next(walk(f'{original_folder}'))
+
+    int_transf_files = list(map(int, re.findall(r'\d+', transf_file.split('_')[0])))
+
+    list_key_vars = pd.read_csv('PPT/list_key_vars.csv')
+    set_key_vars = ast.literal_eval(
+        list_key_vars.loc[list_key_vars['ds']==int_transf_files[0], 'set_key_vars'].values[0])
+
+    orig_file = [file for file in input_files if int(file.split(".csv")[0]) == int_transf_files[0]]
+    print(int_transf_files)
+    orig_data = pd.read_csv(f'{original_folder}/{orig_file[0]}')
+    print(orig_file)
+
+    for i in range(len(set_key_vars)):
+        risk = privacy_risk_smote_under_over(transf_file, orig_data, args, set_key_vars[i], i)
+        total_risk = pd.DataFrame.from_dict(risk)
+        total_risk.to_csv(f'{args.output_folder}/{transf_file.split(".csv")[0]}_qi{i}_per.csv', index=False) 

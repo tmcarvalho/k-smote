cb6784ca8744ba029a6adea46571b541ac6e2980
tmcarvalho <taniammc.94@gmail.com>
2022-09-09 19:52:32 +0100

    change target type

--- code/gaussianCopula.py
+++ code/gaussianCopula.py
@@ -1,14 +1,27 @@
 # %%
-from os import walk
+from os import sep, walk
+import pandas as pd
+from collections import defaultdict
+from sklearn.preprocessing import LabelEncoder
 from sdv.tabular import GaussianCopula
 
 # %%
-def synt_gaussianCopula(data):
+def synt_gaussianCopula(original_folder, data):
+    output_interpolation_folder = '../output/oversampled/gaussianCopula/'
+    data = pd.read_csv(f'{original_folder}/{file}')
+
+    # transform target to string because integer targets are not well synthesised
+    data[data.columns[-1]] = data[data.columns[-1]].astype(str)
+
     model = GaussianCopula()
     model.fit(data)
     new_data = model.sample(num_rows=len(data))
 
-    return new_data
+    # save synthetic data
+    new_data.to_csv(
+        f'{output_interpolation_folder}{sep}ds{file.split(".csv")[0]}_gaussianCopula.csv',
+        index=False)    
+
 
 # %%
 original_folder = '../original'
@@ -20,3 +33,4 @@ for idx,file in enumerate(input_files):
         print(idx)
         print(file)
         synt_gaussianCopula(original_folder, file)
+# %%

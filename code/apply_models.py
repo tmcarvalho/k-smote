6941fa8d8674a3f319963acce4540653dc078158
tmcarvalho <taniammc.94@gmail.com>
2022-09-16 11:23:37 +0100

    add GANs

--- code/apply_models.py
+++ code/apply_models.py
@@ -196,3 +196,42 @@ def modeling_singleouts(file, args):
             raise exc
 
 # %%
+def modeling_gans(file, args):
+    """Apply predictive performance.
+
+    Args:
+        file (string): input file
+    """
+    print(f'{args.input_folder}/{file}')
+
+    indexes = np.load('indexes.npy', allow_pickle=True).item()
+    indexes = pd.DataFrame.from_dict(indexes)
+
+    f = list(map(int, re.findall(r'\d+', file.split('_')[0])))
+    index = indexes.loc[indexes['ds']==str(f[0]), 'indexes'].values[0]
+
+    orig_folder = 'original'
+    _, _, orig_files = next(os.walk(f'{orig_folder}'))
+    orig_file = [file for file in orig_files if list(map(int, re.findall(r'\d+', file.split('_')[0])))[0] == f[0]]
+    print(orig_file)
+    orig_data = pd.read_csv(f'{orig_folder}/{orig_file[0]}')
+    data = pd.read_csv(f'{args.input_folder}/{file}')
+
+    # prepare data to modeling
+    orig_data = orig_data.apply(LabelEncoder().fit_transform)
+    data = data.apply(LabelEncoder().fit_transform)
+    x_train, y_train = data.iloc[:, :-1], data.iloc[:, -1]
+
+    x_test = orig_data.iloc[index, :-1]
+    y_test = orig_data.iloc[index, -1]
+
+    if y_train.value_counts().nunique() != 1:
+        # predictive performance
+        validation, test = evaluate_model(x_train, x_test, y_train, y_test)
+        # save validation and test results
+        try:
+            if validation:
+                save_results(file, args, validation, test)
+
+        except Exception as exc:
+            raise exc

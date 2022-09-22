38379fbbbedc47a9530b70301a5b4b00b69ead7e
tmcarvalho <taniammc.94@gmail.com>
2022-09-22 15:40:15 +0100

    fix "under" test files

--- code/apply_models.py
+++ code/apply_models.py
@@ -100,29 +100,39 @@ def modeling_smote_under_over(file, args):
     data = pd.read_csv(f'{args.input_folder}/{file}')
     print(f)
     if 'under' in file:
-        orig_folder = 'original'
-        _, _, orig_files = next(os.walk(f'{orig_folder}'))
+        # orig_folder = 'original'
+        # _, _, orig_files = next(os.walk(f'{orig_folder}'))
  
-        orig_file = [file for file in orig_files if list(map(int, re.findall(r'\d+', file.split('_')[0])))[0] == f[0]]
+        #orig_file = [file for file in orig_files if list(map(int, re.findall(r'\d+', file.split('_')[0])))[0] == f[0]]
         
-        orig_data = pd.read_csv(f'{orig_folder}/{orig_file[0]}')
-        test_data = orig_data.iloc[index,:]
-        print(len(data))
-        print(len(test_data))
-        dups = pd.concat([data, test_data])
-        dups = dups[dups.duplicated(keep='first')]
-        print(len(dups))
-        if len(dups) != 0:
-            dups_index = list(dups.index)
-            index = list(set(index)-set(dups_index))
-            print(len(index))
-        # prepare data to modeling
+        #orig_data = pd.read_csv(f'{orig_folder}/{orig_file[0]}')
+        # test_data = orig_data.iloc[index,:]
+        # print(len(data))
+        # print(len(test_data))
+        # dups = pd.concat([data, test_data])
+        # dups = dups[dups.duplicated(keep='first')]
+        # print(len(dups))
         data = data.apply(LabelEncoder().fit_transform)
-        orig_data = orig_data.apply(LabelEncoder().fit_transform)
 
-        x_train, y_train = data.iloc[:, :-1], data.iloc[:, -1]
-        x_test = orig_data.iloc[index, :-1]
-        y_test = orig_data.iloc[index, -1]
+        X, y = data.iloc[:, :-1], data.iloc[:, -1]
+        # split data 80/20
+        train_idx = list(set(list(X.index)) - set(index))
+        train_idx = [i for i in train_idx if i in X.index]
+        test_idx = [i for i in index if i in X.index]
+        x_train = X.iloc[train_idx, :]
+        x_test = X.iloc[test_idx, :]
+        y_train = y[train_idx]
+        y_test = y[test_idx]
+        
+        print(len(index))
+        print(len(test_idx))
+        # prepare data to modeling
+        # data = data.apply(LabelEncoder().fit_transform)
+        #orig_data = orig_data.apply(LabelEncoder().fit_transform)
+
+        #x_train, y_train = data.iloc[:, :-1], data.iloc[:, -1]
+        #x_test = data.iloc[index, :-1]
+        #y_test = orig_data.iloc[index, -1]
     
     else:
         print(len(index))
@@ -143,7 +153,7 @@ def modeling_smote_under_over(file, args):
 
     # save validation and test results
     try:
-       save_results(file, args, validation, test)
+        save_results(file, args, validation, test)
 
     except Exception as exc:
         raise exc

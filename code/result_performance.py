00708a86e570e6e2969c7d10b0d6642e5093baba
tmcarvalho <taniammc.94@gmail.com>
2022-10-06 16:43:44 +0100

    results on test

--- code/result_performance.py
+++ code/result_performance.py
@@ -1,329 +1,62 @@
-"""Re-identification of single outs
-This script will analyse the re-identification risk with
-record linkage for single outs.
+"""Predictive performance analysis
+This script will analyse the predictive performance in the out-of-sample.
 """
 # %%
 import os
-from os import walk
-import re
-import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 
+# %% 'all_results' are processed in the bayesTest file
+all_results = pd.read_csv('../output/bayesianTest_baseline_org_auc.csv')
 # %%
-def transform_npy_to_csv(original_foler, folder_validation):
-    """Transform npy to csv with percentage difference for eeach metric.
+results_max = all_results.groupby(['ds', 'technique'], as_index=False)['test_roc_auc_perdif'].max()
 
-    Args:
-        original_foler (string): path to original data folder
-        folder_validation (string): path to validatin results
-    """
-    _, _, performance_files = next(walk(f'{folder_validation}/'))
-    performance_files.sort()
-    _, _, orig_file_validation = next(walk(f'{original_foler}/'))
-    orig_file_validation.sort()
-    metrics = ['gmean', 'acc', 'bal_acc', 'f1', 'f1_weighted', 'roc_auc_curve']
-
-    for org in orig_file_validation:
-        o = list(map(int, re.findall(r'\d+', org.split('.')[0])))[0]
-        if o not in [0,1,3,13,23,28,34,36,40,48,54,66,87]:
-            for file in performance_files:
-                if 'npy' in org and 'npy' in file:
-                    org_result = np.load(f'{original_foler}/{org}', allow_pickle='TRUE').item()
-                    org_result = pd.DataFrame.from_dict(org_result)
-                    t = list(map(int, re.findall(r'\d+', file.split('.')[0])))[0]
-                    if o==t:
-                        result = np.load(f'{folder_validation}/{file}', allow_pickle='TRUE').item()
-                        result = pd.DataFrame.from_dict(result)
-                        # add original results
-                        for i in range(0, 33):
-                            result.loc[i, 'mean_test_original'] = org_result['mean_test_f1_weighted'][i]
-
-
-                        # for each CV calculate the percentage difference
-                        for metric in metrics:
-                            # 100 * (Sc - Sb) / Sb
-                            max_rf = org_result.loc[org_result['param_classifier']==org_result.param_classifier.unique()[0], 'mean_test_' + metric].max()
-                            max_xgb = org_result.loc[org_result['param_classifier']==org_result.param_classifier.unique()[1], 'mean_test_' + metric].max()
-                            max_logr = org_result.loc[org_result['param_classifier']==org_result.param_classifier.unique()[2], 'mean_test_' + metric].max()
-                        
-                            result.loc[result['param_classifier']==result.param_classifier.unique()[0], 'mean_test_' + metric + '_perdif'] = 100 * (result['mean_test_' + metric] - max_rf) / max_rf
-                            result.loc[result['param_classifier']==result.param_classifier.unique()[1], 'mean_test_' + metric + '_perdif'] = 100 * (result['mean_test_' + metric] - max_xgb) / max_xgb
-                            result.loc[result['param_classifier']==result.param_classifier.unique()[2], 'mean_test_' + metric + '_perdif'] = 100 * (result['mean_test_' + metric] - max_logr) / max_logr
-                        
-                        result.loc[result['param_classifier']==result.param_classifier.unique()[0], 'model'] = "Random Forest"
-                        result.loc[result['param_classifier']==result.param_classifier.unique()[1], 'model'] = "XGBoost"
-                        result.loc[result['param_classifier']==result.param_classifier.unique()[2], 'model'] = "Logistic Regression"
-
-                        # save csv with the 5 algorithms
-                        result.to_csv(f'{folder_validation}/{file.replace(".npy", ".csv")}', index=False)
-
-# %%
-original_folder = f'{os.path.dirname(os.getcwd())}/output/modeling/original/validation'
-# %%
-#transform_npy_to_csv(original_folder, f'{os.path.dirname(os.getcwd())}/output/modeling/PPT/validation')
-transform_npy_to_csv(original_folder, f'{os.path.dirname(os.getcwd())}/output/modeling/PPT_ARX/validation')
-# %%
-transform_npy_to_csv(original_folder, f'{os.path.dirname(os.getcwd())}/output/modeling/smote_under_over/validation')
-# %%
-transform_npy_to_csv(original_folder, f'{os.path.dirname(os.getcwd())}/output/modeling/smote_singleouts/validation')
-transform_npy_to_csv(original_folder, f'{os.path.dirname(os.getcwd())}/output/modeling/smote_singleouts_scratch/validation')
-transform_npy_to_csv(original_folder, f'{os.path.dirname(os.getcwd())}/output/modeling/borderlineSmote_singleouts/validation')
-# %%
-transform_npy_to_csv(original_folder, f'{os.path.dirname(os.getcwd())}/output/modeling/borderlineSmote/validation')
-# %%
-transform_npy_to_csv(original_folder, f'{os.path.dirname(os.getcwd())}/output/modeling/gaussianCopula/validation')
-# %%
-transform_npy_to_csv(original_folder, f'{os.path.dirname(os.getcwd())}/output/modeling/copulaGAN/validation')
-transform_npy_to_csv(original_folder, f'{os.path.dirname(os.getcwd())}/output/modeling/CTGAN/validation')
-transform_npy_to_csv(original_folder, f'{os.path.dirname(os.getcwd())}/output/modeling/TVAE/validation')
-# %%
-def concat_ppt_results():
-    """Join all csv results for privacy preserving techniques
-
-    Returns:
-        pd.Dataframe: joined results
-    """
-    # folder_validation = f'{os.path.dirname(os.getcwd())}/output/modeling/PPT/validation'
-    folder_validation = f'{os.path.dirname(os.getcwd())}/output/modeling/PPT_ARX/validation'
-    results = []
-    _, _, performance_files = next(walk(f'{folder_validation}/'))
-    performance_files.sort()
-    performance_files = [file for file in performance_files if '.csv' in file]
-
-    for _, file in enumerate(performance_files):
-        f = list(map(int, re.findall(r'\d+', file.split('_')[0])))[0]
-        if f not in [0,1,3,13,23,28,34,36,40,48,54,66,87]:
-            if '.csv' in file:
-                result = pd.read_csv(f'{folder_validation}/{file}')
-                quasi_id_int = file.split('_')[1]
-                quasi_id_int = list(map(int, re.findall(r'\d+', quasi_id_int)))[0]
-                quasi_id = f'Set of quasi-identifiers #{quasi_id_int+1}'
-                result['qi'] = quasi_id
-                result['ds'] = file.split('_')[0]
-                result['technique'] = 'PPT'
-                result['knn'] = None
-                result['per'] = None
-                result['ds_complete'] = file
-
-                results.append(result)
-
-    concat_results = pd.concat(results)
-
-    return concat_results
-
-
-def concat_smote_under_over_results():
-    """Join all csv results for smote, under and over
-
-    Returns:
-        pd.Dataframe: joined results
-    """
-    folder_validation = f'{os.path.dirname(os.getcwd())}/output/modeling/smote_under_over/validation'
-    results = []
-    _, _, performance_files = next(walk(f'{folder_validation}/'))
-    performance_files.sort()
-    performance_files = [file for file in performance_files if '.csv' in file]
-
-    for _, file in enumerate(performance_files):
-        f = list(map(int, re.findall(r'\d+', file.split('_')[0])))[0]
-        if f not in [0,1,3,13,23,28,34,36,40,48,54,66,87]:
-            if '.csv' in file:
-                result = pd.read_csv(f'{folder_validation}/{file}')
-                per = file.split('_')[2]
-                per = per.replace(".csv", "")
-                per = list(map(float, re.findall(r'\d+\.\d+', per)))[0] if '.' in per else list(map(float, re.findall(r'\d', per)))[0]
-                result['technique'] = file.split('_')[1].title()
-                if file.split('_')[1] == 'smote':
-                    result['knn'] = list(map(int, re.findall(r'\d+', file.split('_')[2])))[0]
-                result['per'] = per
-                result['ds'] = file.split('_')[0]
-                result['qi'] = None
-                result['ds_complete'] = file
-
-                results.append(result)
-
-    concat_results = pd.concat(results)
-
-    return concat_results
-
-
-def concat_gans(folder_validation, technique):
-    """Join all csv results for GANs
-
-    Returns:
-        pd.Dataframe: joined results
-    """
-    results = []
-    _, _, performance_files = next(walk(f'{folder_validation}/'))
-    performance_files.sort()
-    performance_files = [file for file in performance_files if '.csv' in file]
-
-    for _, file in enumerate(performance_files):
-        f = list(map(int, re.findall(r'\d+', file.split('_')[0])))[0]
-        if f not in [0,1,3,13,23,28,34,36,40,48,54,66,87]:
-            if '.csv' in file:
-                result = pd.read_csv(f'{folder_validation}/{file}')
-                result['technique'] = technique
-                result['ds'] = file.split('_')[0]
-                result['ds_complete'] = file
-
-                results.append(result)
-
-    concat_results = pd.concat(results)
-
-    return concat_results
-
-
-def concat_borderlinesmote():
-    """Join all csv results for borderlineSmote
-
-    Returns:
-        pd.Dataframe: joined results
-    """
-    folder_validation = f'{os.path.dirname(os.getcwd())}/output/modeling/borderlineSmote/validation'
-    results = []
-    _, _, performance_files = next(walk(f'{folder_validation}/'))
-    performance_files.sort()
-    performance_files = [file for file in performance_files if '.csv' in file]
-
-    for _, file in enumerate(performance_files):
-        f = list(map(int, re.findall(r'\d+', file.split('_')[0])))[0]
-        if f not in [0,1,3,13,23,28,34,36,40,48,54,66,87]:
-            if '.csv' in file:
-                result = pd.read_csv(f'{folder_validation}/{file}')
-                per = file.split('_')[2]
-                per = per.replace(".csv", "")
-                per = list(map(float, re.findall(r'\d+\.\d+', per)))[0] if '.' in per else list(map(float, re.findall(r'\d', per)))[0]
-                result['technique'] = 'BorderlineSMOTE'
-                result['knn'] = list(map(int, re.findall(r'\d+', file.split('_')[2])))[0]
-                result['per'] = per
-                result['ds'] = file.split('_')[0]
-                result['qi'] = None
-                result['ds_complete'] = file
-
-                results.append(result)
-
-    concat_results = pd.concat(results)
-
-    return concat_results
-
-
-def concat_results_smote_singleouts(folder_validation, technique):
-    """Join all csv results for smote with single outs
-
-    Args:
-        technique (string): distinction between smote techniques
-
-    Returns:
-        pd.Dataframe: joined results
-    """
-    results = []
-    _, _, performance_files = next(walk(f'{folder_validation}/'))
-    performance_files.sort()
-    performance_files = [file for file in performance_files if '.csv' in file]
-
-    for _, file in enumerate(performance_files):
-        f = list(map(int, re.findall(r'\d+', file.split('_')[0])))[0]
-        if f not in [0,1,3,13,23,28,34,36,40,48,54,66,87]:
-            if '.csv' in file:
-                result = pd.read_csv(f'{folder_validation}/{file}')
-                quasi_id_int = file.split('_')[2]
-                quasi_id_int = list(map(int, re.findall(r'\d+', quasi_id_int)))[0]
-                quasi_id = f'Set of quasi-identifiers #{quasi_id_int+1}'
-                k_nn = file.split('_')[3]
-                k_nn = list(map(int, re.findall(r'\d+', k_nn)))[0]
-                per = file.split('_')[4]
-                per = per.replace(".csv", "")
-                per = per = list(map(float, re.findall(r'\d+\.\d+', per)))[0] if '.' in per else list(map(float, re.findall(r'\d', per)))[0]
-                result['qi'] = quasi_id
-                result['knn'] = k_nn
-                result['per'] = per
-                result['ds'] = file.split('_')[0]
-                result['technique'] = technique
-                result['ds_complete'] = file
-
-                results.append(result)
-
-    concat_results = pd.concat(results)
-
-    return concat_results
-
-# %%
-ppt_results = concat_ppt_results()
-# %%
-smote_under_over_results = concat_smote_under_over_results()
-# %%
-bordersmote = concat_borderlinesmote()
-# %%
-gaussianCopula = concat_gans(
-    f'{os.path.dirname(os.getcwd())}/output/modeling/gaussianCopula/validation',
-    'Gaussian Copula') 
-copulaGAN = concat_gans(
-    f'{os.path.dirname(os.getcwd())}/output/modeling/copulaGAN/validation',
-    'Copula GAN') 
-tvae = concat_gans(
-    f'{os.path.dirname(os.getcwd())}/output/modeling/TVAE/validation',
-    'TVAE') 
-ctgan = concat_gans(
-    f'{os.path.dirname(os.getcwd())}/output/modeling/CTGAN/validation',
-    'CTGAN') 
-# %%
-smote_singleouts_oneclass = concat_results_smote_singleouts(
-    f'{os.path.dirname(os.getcwd())}/output/modeling/smote_singleouts/validation',
-    'privateSMOTE')
-smote_singleouts_twoclasses = concat_results_smote_singleouts(
-    f'{os.path.dirname(os.getcwd())}/output/modeling/smote_singleouts_scratch/validation',
-    'privateSMOTE \n regardless of \n the class')
-#bordersmote_singleouts_oneclass = concat_results_smote_singleouts(
-#    f'{os.path.dirname(os.getcwd())}/output/modeling/borderlineSmote_singleouts/validation',
-#    'privateBorderlineSMOTE')   
-
-# %%
-all_results = pd.concat([ppt_results, smote_under_over_results, bordersmote, gaussianCopula, copulaGAN, tvae, ctgan, smote_singleouts_oneclass, smote_singleouts_twoclasses])
-# %%
-# all_results.to_csv('../output/predictiveresults.csv', index=False)
-# all_results = all_results.read_csv('../output/predictiveresults.csv')
-# %%
-results_max = all_results.groupby(['ds', 'technique'], as_index=False)['mean_test_f1_weighted', 'mean_test_f1_weighted_perdif', 'mean_test_gmean_perdif', 'mean_test_roc_auc_curve_perdif'].max()
-
-# %%
-order = ['PPT', 'RUS', 'SMOTE', 'BorderlineSMOTE', 'Gaussian Copula', 'Copula GAN', 'TVAE', 'CTGAN', 'privateSMOTE', 'privateSMOTE \n regardless of \n the class']
 # %%
 results_max = results_max.loc[results_max['technique']!='Over']
 results_max.loc[results_max['technique']=='Under', 'technique'] = 'RUS'
 results_max.loc[results_max['technique']=='Smote', 'technique'] = 'SMOTE'
+results_max.loc[results_max['technique']=='privateSMOTE', 'technique'] = 'privateSMOTE A'
+results_max.loc[results_max['technique']=='privateSMOTE \n regardless of \n the class', 'technique'] = 'privateSMOTE B'
+
+# %%
+order = ['PPT', 'RUS', 'SMOTE', 'BorderlineSMOTE', 'Copula GAN', 'TVAE', 'CTGAN', 'privateSMOTE A', 'privateSMOTE B']
 # %%
 sns.set_style("darkgrid")
 plt.figure(figsize=(12,8))
-ax = sns.boxplot(data=results_max, x='technique', y='mean_test_f1_weighted_perdif', palette='Spectral_r', order=order)
-# ax.set(ylim=(0, 30))
+ax = sns.boxplot(data=results_max, x='technique', y='test_roc_auc_perdif', palette='Spectral_r', order=order)
+# ax.set(ylim=(-60, 30))
+#ax.set_yscale("log")
 sns.set(font_scale=1.5)
 plt.xticks(rotation=45)
 plt.xlabel("")
 plt.ylabel("Percentage difference of predictive performance (F-score)")
-plt.show()
-#figure = ax.get_figure()
-#figure.savefig(f'{os.path.dirname(os.getcwd())}/output/plots/alltechniques.pdf', bbox_inches='tight')
-
-# %%
-melted_results = results_max.melt(id_vars=['technique'], value_vars=["mean_test_f1_weighted_perdif","mean_test_gmean_perdif", "mean_test_roc_auc_curve_perdif"], 
-        var_name="Measure", 
-        value_name="Value")
-
-# %%
-plt.figure(figsize=(20,16))
-ax = sns.boxplot(data=melted_results, x='technique', y='Value', hue='Measure', palette='muted', order=order)
-ax.set(yscale='log')
-sns.set(font_scale=1.5)
-#plt.ylim(-10**1, 10**3)
-#ax.set(ylim=(-10**2, 10**3))
-plt.xticks(rotation=30)
-plt.xlabel("")
-plt.ylabel("Percentage difference of predictive performance (F-score)")
+plt.yscale('symlog')
+plt.autoscale(True)
 plt.show()
 # figure = ax.get_figure()
-# figure.savefig(f'{os.path.dirname(os.getcwd())}/output/plots/allresults_technique.pdf', bbox_inches='tight')
+# figure.savefig(f'{os.path.dirname(os.getcwd())}/output/plots/alltechniques_outofsample.pdf', bbox_inches='tight')
+
+
+# %%
+results_max_privateSMOTE = results_max.loc[results_max.technique!='privateSMOTE A']
+results_max_privateSMOTE.loc[results_max_privateSMOTE['technique']=='privateSMOTE B', 'technique'] = 'privateSMOTE'
+order = ['PPT', 'RUS', 'SMOTE', 'BorderlineSMOTE', 'Copula GAN', 'TVAE', 'CTGAN', 'privateSMOTE']
+sns.set_style("darkgrid")
+plt.figure(figsize=(15,11))
+ax = sns.boxplot(data=results_max_privateSMOTE, x='technique', y='test_roc_auc_perdif', palette='Spectral_r', order=order)
+ax.margins(x=0.03)
+ax.margins(y=0.08)
+ax.use_sticky_edges = False
+ax.autoscale_view(scaley=True)
+sns.set(font_scale=2)
+plt.xticks(rotation=45)
+plt.xlabel("")
+plt.ylabel("Percentage difference of predictive performance (AUC)")
+plt.yscale('symlog')
+plt.autoscale(True)
+plt.show()
+figure = ax.get_figure()
+figure.savefig(f'{os.path.dirname(os.getcwd())}/output/plots/alltechniques_outofsample_auc.pdf', bbox_inches='tight')
 
 # %%

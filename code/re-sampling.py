2d3fb47c0371a887c8364a3a46882657c1dae36e
tmcarvalho <taniammc.94@gmail.com>
2022-10-07 13:39:27 +0100

    one function for privateSMOTE, resampling and gans

--- code/re-sampling.py
+++ code/re-sampling.py
@@ -4,10 +4,12 @@ This script interpolate data using smote, under and over techniques.
 # %%
 from os import walk
 import pandas as pd
+import numpy as np
+import re
 from collections import defaultdict
 from sklearn.preprocessing import LabelEncoder
 from imblearn.under_sampling import RandomUnderSampler
-from imblearn.over_sampling import SMOTE, RandomOverSampler
+from imblearn.over_sampling import SMOTE, BorderlineSMOTE
 
 
 def interpolation(original_folder, file):
@@ -17,9 +19,18 @@ def interpolation(original_folder, file):
         original_folder (string): path of original folder
         file (string): name of file
     """
-    output_interpolation_folder = '../output/oversampled/smote_under_over'
+    output_interpolation_folder = '../output/oversampled/re-sampling'
 
     data = pd.read_csv(f'{original_folder}/{file}')
+    # get 80% of data to synthesise
+    indexes = np.load('../indexes.npy', allow_pickle=True).item()
+    indexes = pd.DataFrame.from_dict(indexes)
+
+    f = list(map(int, re.findall(r'\d+', file.split('_')[0])))
+    index = indexes.loc[indexes['ds']==str(f[0]), 'indexes'].values[0]
+    data_idx = list(set(list(data.index)) - set(index))
+    data = data.iloc[data_idx, :]
+
     label_encoder_dict = defaultdict(LabelEncoder)
     data_encoded = data.apply(lambda x: label_encoder_dict[x.name].fit_transform(x))
     map_dict = dict()
@@ -34,7 +45,6 @@ def interpolation(original_folder, file):
     knn = [1, 3, 5]
     # percentage of majority and minority class
     ratios_smote = [0.5, 0.75, 1]
-    ratios_over = [0.5, 0.75, 1]
     ratios_under = [0.25, 0.5, 0.75, 1]
     for nn in knn:
         for smote in ratios_smote:
@@ -42,43 +52,29 @@ def interpolation(original_folder, file):
                 smote_samp = SMOTE(random_state=42,
                             k_neighbors=nn,
                             sampling_strategy=smote)
+                border_smote = BorderlineSMOTE(random_state=42,
+                            k_neighbors=nn,
+                            sampling_strategy=smote)
                 # fit predictor and target variable
                 X = data_encoded[data_encoded.columns[:-1]]
                 y = data_encoded.iloc[:, -1]
                 x_smote, y_smote = smote_samp.fit_resample(X, y)
+                x_bordersmote, y_bordersmote = border_smote.fit_resample(X, y)
                 
                 # add target
                 x_smote[data_encoded.columns[-1]] = y_smote
+                x_bordersmote[data_encoded.columns[-1]] = y_bordersmote
 
                 # decoded
                 for key in map_dict.keys():  
                     d = dict(map(reversed, map_dict[key].items()))
                     x_smote[key] = x_smote[key].map(d)
+                    x_bordersmote[key] = x_bordersmote[key].map(d)
 
                 x_smote.to_csv(f'{output_interpolation_folder}/ds{file.split(".csv")[0]}_smote_knn{nn}_per{smote}.csv', index=False)
+                x_bordersmote.to_csv(f'{output_interpolation_folder}/ds{file.split(".csv")[0]}_bordersmote_knn{nn}_per{smote}.csv', index=False)
 
-            except: pass        
-
-    for over in ratios_over:
-        try:
-            over_samp = RandomOverSampler(random_state=42,
-                        sampling_strategy=over)
-            # fit predictor and target variable
-            X = data_encoded[data_encoded.columns[:-1]]
-            y = data_encoded.iloc[:, -1]
-            x_over, y_over = over_samp.fit_resample(X, y)
-
-            # add target
-            x_over[data_encoded.columns[-1]] = y_over
-
-            # decoded
-            for key in map_dict.keys():  
-                d = dict(map(reversed, map_dict[key].items()))
-                x_over[key] = x_over[key].map(d)
-
-            x_over.to_csv(f'{output_interpolation_folder}/ds{file.split(".csv")[0]}_over_per{over}.csv', index=False)
-
-        except: pass    
+            except: pass         
     
     for under in ratios_under:
         try:

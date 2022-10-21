f784fb80ba3fc47af24a4030f08c5c9e2582a1f3
tmcarvalho <taniammc.94@gmail.com>
2022-10-21 09:41:09 +0100

    re-calculate single outs in transf data

--- code/apply_record_linkage.py
+++ code/apply_record_linkage.py
@@ -24,8 +24,8 @@ def privacy_risk_privatesmote_and_ppts(transf_file, orig_data, args, list_key_va
     key_vars = ast.literal_eval(set_key_vars)[int_transf_qi[0]]
     
     if args.type == 'smote_singleouts':
-        # select single outs in the transformed data
-        transf_data = transf_data[transf_data['single_out']==1]
+        # re-calculate single outs after the oversampling and select them
+        transf_data = aux_singleouts(key_vars, transf_data)
         # select single outs in the original data according the set of key vars
         orig_data = aux_singleouts(key_vars, orig_data)
     

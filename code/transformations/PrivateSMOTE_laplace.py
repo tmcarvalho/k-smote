704ee6e977bfda997d3f035b331e994e0677d31f
tmcarvalho <taniammc.94@gmail.com>
2023-05-13 13:19:17 -0400

    add PrivateSMOTE with laplace

---
+++ code/transformations/PrivateSMOTE_laplace.py
@@ -0,0 +1,233 @@
+"""Apply interpolation with SMOTE
+This script will apply SMOTE technique in the single out cases.
+"""
+# %%
+import psutil
+from os import sep
+import re
+import ast
+import pandas as pd
+import numpy as np
+import random
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import StandardScaler
+import random
+from random import randrange
+import time
+
+
+def keep_numbers(data):
+    """mantain correct data types according to the data"""
+    data_types = data.copy()
+    for col in data.columns:
+        # transform strings to digits
+        if isinstance(data[col].iloc[0], str) and data[col].iloc[0].isdigit():
+            data[col] = data[col].astype(float)
+        # remove trailing zeros
+        if isinstance(data[col].iloc[0], (int, float)):
+            if int(data[col].iloc[0]) == float(data[col].iloc[0]):
+                data_types[col] = data[col].astype(int)
+            else: data[col] = data_types[col].astype(float)
+    return data, data_types
+
+
+def aux_singleouts(key_vars, dt):
+    """create single out variable based on k-anonymity"""
+    k = dt.groupby(key_vars)[key_vars[0]].transform(len)
+    dt['single_out'] = np.where(k == 1, 1, 0)
+    return dt
+
+
+class Smote:
+    """Apply Smote
+    """
+    def __init__(self, samples, singleout, y, N, k, ep):
+        """Initiate arguments
+
+        Args:
+            samples (pd.Dataframe): training samples
+            singleout (pd.Series): singleout variable
+            y (pd.Series): target sample
+            N (int): number of interpolations per observation
+            k (int): number of nearest neighbours
+            ep (float): privacy budget (epsilon)
+        """
+        self.samples = samples.reset_index(drop=True)
+        self.n_samples = samples.shape[0]
+        self.n_attrs = samples.shape[1]
+        self.singleout = singleout.reset_index(drop=True)
+        self.y = y
+        self.N = N
+        self.k = k
+        self.ep = ep
+        self.newindex=0
+        # transform all data in ndarray with the same dtypes
+        self.x = np.array(self.samples, dtype=self.samples.dtypes)
+
+    def over_sampling(self):
+        """find the nearest neighbors and populate with new data
+
+        Returns:
+            pd.DataFrame: synthetic data
+        """
+        N = int(self.N)
+
+        # OHE + standardization for nearest neighbor
+        encoded_data = pd.get_dummies(self.samples, drop_first=True).astype(int)
+        standardized_data = StandardScaler().fit_transform(encoded_data)
+        neighbors = NearestNeighbors(n_neighbors=self.k+1).fit(standardized_data)
+
+        # transform the tager in 1D-array
+        self.y = np.array(self.y)
+
+        # inicialize the synthetic samples
+        self.synthetic = np.empty(shape=(self.n_samples * N, self.n_attrs+1), dtype=self.samples.dtypes)
+        
+        # find the categories for each categorical column in all sample
+        self.unique_values = [self.samples.loc[:,col].unique() for col in self.samples.select_dtypes(object)]
+
+        # find the minimun value for each numerical column: TODO not used line
+        print("MIN VALUES of original")
+        self.min_values = [self.samples[col].min() if not isinstance(self.samples[col].iloc[0], str) else np.nan for col in self.samples.columns]
+        print( self.min_values)
+        print("MAX VALUES of original")
+        self.max_values = [self.samples[col].max() if not isinstance(self.samples[col].iloc[0], str) else np.nan for col in self.samples.columns]
+        print( self.max_values)
+        # find the mean values for each numerical column
+        self.mean_values = [self.samples[col].mean() if not isinstance(self.samples[col].iloc[0], str) else np.nan for col in self.samples.columns]
+
+        # for each observation find nearest neighbours
+        for i in range(len(self.samples)):
+            nnarray = neighbors.kneighbors(
+                standardized_data[i].reshape(1, -1), return_distance=False)[0]
+            self._populate(N, i, nnarray)
+
+        return self.synthetic
+
+    def _populate(self, N, i, nnarray):
+        # populate N times
+        while N!=0:
+            # find index of nearest neighbour excluding the observation in comparison
+            neighbour = randrange(1, self.k+1)
+
+            # control noise at tails
+            control_noise = [(np.multiply(neighbor_val-orig_val, np.random.laplace(0, 1/self.ep, size=None)) if (isinstance(orig_val, (int, float))) \
+                                      else orig_val) \
+                                    for (neighbor_val, orig_val) in zip(self.x[nnarray[neighbour]], self.x[i])]
+
+            # generate new numerical value for each column
+            new_nums_values = [orig_val + control_noise[j] if (isinstance(orig_val, (int, float)) and 
+                                self.min_values[j] <= orig_val + control_noise[j] <= self.max_values[j]) \
+                                      else orig_val - control_noise[j] if isinstance(orig_val, (int, float)) \
+                                else orig_val for j, orig_val in enumerate(self.x[i])]
+
+            if len(self.unique_values) > 0:
+                # find the categories for each categorical column in nearest neighbors sample
+                nn_unique = [self.samples.loc[nnarray[1:self.k+1],col].unique() for col in self.samples.select_dtypes(object)]
+
+                # randomly select a category
+                new_cats_values = [random.choice(self.unique_values[u]) if len(nn_unique[u]) == 1 else random.choice(nn_unique[u]) for u in range(len(self.unique_values))]
+        
+                # replace the old categories
+                iter_cat_calues = iter(new_cats_values)
+                new_nums_values = [next(iter_cat_calues) if isinstance(val, str) else val for val in new_nums_values]
+            
+            # assign interpolated values
+            self.synthetic[self.newindex, 0:len(new_nums_values)] = new_nums_values
+
+            # assign intact target variable
+            self.synthetic[self.newindex, len(new_nums_values)] = self.y[i]
+            self.newindex+=1
+
+            N-=1
+
+# %% privateSMOTE with "force" - add 1 or -1 when difference is 0
+def PrivateSMOTE_laplace_(msg):
+    """Generate several interpolated data sets considering all classes.
+
+    Args:
+        msg (str): name of the original file and respective PrivateSMOTE parameters
+    """
+    print(msg)
+
+    start= time.time()
+    output_interpolation_folder = 'output/oversampled/PrivateSMOTE_laplace'
+    
+    # get 80% of data to synthesise
+    indexes = np.load('indexes.npy', allow_pickle=True).item()
+    indexes = pd.DataFrame.from_dict(indexes)
+
+    f = list(map(int, re.findall(r'\d+', msg.split('_')[0])))
+    print(str(f[0]))
+    data = pd.read_csv(f'original/{str(f[0])}.csv')
+
+    index = indexes.loc[indexes['ds']==str(f[0]), 'indexes'].values[0]
+    data_idx = list(set(list(data.index)) - set(index))
+    data = data.iloc[data_idx, :]
+
+    # encode string with numbers to numeric and remove trailing zeros
+    data, data_types = keep_numbers(data)
+    
+    list_key_vars = pd.read_csv('list_key_vars.csv')
+    set_key_vars = ast.literal_eval(
+        list_key_vars.loc[list_key_vars['ds']==f[0], 'set_key_vars'].values[0])
+
+    keys_nr = list(map(int, re.findall(r'\d+', msg.split('_')[2])))[0]
+    print(keys_nr)
+    keys = set_key_vars[keys_nr]
+    data = aux_singleouts(keys, data)
+
+    X_train = data.loc[data['single_out']==1, data.columns[:-2]] # training singleout sample
+    Y_train = data.loc[data['single_out']==1, data.columns[-1]] # singleout variable values
+    y = data.loc[data['single_out']==1, data.columns[-2]] # target variable values
+
+    knn = list(map(int, re.findall(r'\d+', msg.split('_')[3])))[0]
+    per = list(map(int, re.findall(r'\d+', msg.split('_')[4])))[0]
+    ep = list(map(int, re.findall(r'\d+', msg.split('_')[5])))[0]
+    print(ep)
+    if X_train.shape[0] > 0 and X_train.shape[0] >= knn:
+        new = Smote(X_train, Y_train, y, per, knn, ep).over_sampling()
+        
+        newDf = pd.DataFrame(new, columns = data.columns[:-1])
+        newDf = newDf.astype(dtype = data[data.columns[:-1]].dtypes)
+
+        # assign singleout
+        newDf['single_out'] = 1
+
+        # add non single outs
+        if newDf.shape[0] != data.shape[0]:
+            newDf = pd.concat([newDf, data.loc[data['single_out']==0]]) 
+
+        for col in newDf.columns[:-1]:
+            if data_types[col].dtype == np.int64:
+                newDf[col] = round(newDf[col], 0).astype(int)
+            if data_types[col].dtype == np.float64:
+                # get decimal places in float
+                dec = str(data[col].values[0])[::-1].find('.')
+                newDf[col] = round(newDf[col], dec)
+        print("MIN VALUES of transfomerd")
+        minvalues = [newDf[col].min() if not isinstance(newDf[col].iloc[0], str) else np.nan for col in newDf.columns]
+        print(minvalues)
+        print("MAX VALUES of transfomerd")
+        maxvalues = [newDf[col].max() if not isinstance(newDf[col].iloc[0], str) else np.nan for col in newDf.columns]
+        print(maxvalues)
+
+        # save oversampled data
+        newDf.to_csv(
+            f'{output_interpolation_folder}{sep}{msg}.csv',
+            index=False)
+
+        # Store execution costs  
+        process = psutil.Process()
+        computational_costs = {'execution_time': time.time()-start,
+                                'memory': process.memory_percent(),
+                                'cpu_time_user': process.cpu_times()[0],
+                                'cpu_time_system': process.cpu_times()[1],
+                                'cpu_percent': process.cpu_percent()}
+        
+        computational_costs_df = pd.DataFrame([computational_costs])
+        computational_costs_df.to_csv(
+                f'computational_costs{sep}PrivateSMOTE_laplace{sep}{msg}.csv',
+                index=False)
+
+# %%
---
+++ code/transformations/task_privateSMOTE_laplace.py
@@ -0,0 +1,60 @@
+"""Task manager
+This script will add the tasks in the queue
+"""
+#!/usr/bin/env python
+import argparse
+import os
+import pika
+
+parser = argparse.ArgumentParser(description='Master Example')
+parser.add_argument('--input_folder', type=str, help='Input folder', default="./input")
+args = parser.parse_args()
+
+
+def put_file_queue(ch, file_name):
+    """Add files to the queue
+
+    Args:
+        ch (_type_): channel of the queue
+        file_name (string): name of file
+    """
+    ch.basic_publish(
+        exchange='',
+        routing_key='task_queue_privatesmote_laplace',
+        body=file_name,
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # make message persistent
+        ))
+
+
+credentials = pika.PlainCredentials('guest', 'guest')
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost', credentials=credentials, heartbeat=5))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='dlx', exchange_type='direct')
+
+channel.queue_declare(queue='task_queue_privatesmote_laplace', durable=True, arguments={"dead-letter-exchange": "dlx"})
+dl_queue = channel.queue_declare(queue='dl')
+
+channel.queue_bind(exchange='dlx', routing_key='task_queue_privatesmote_laplace', queue=dl_queue.method.queue)
+
+knn = [1,3,5]
+per = [1,2,3]
+epislon = [1, 2, 3, 5]
+
+files = files = next(os.walk(args.input_folder))[2]
+for file in files:
+    f = int(file.split('.')[0])
+    if f not in [0,1,3,13,23,28,34,36,40,48,54,66,87]:
+        if f in [68]:
+            print(file)
+            for idx in range(5):
+                for k in knn:
+                    for p in per:
+                        for ep in epislon:
+                            # files.append(f'{file.split(".")[0]}_privateSMOTE_QI{idx}_knn{k}_per{p}')
+                            print(f'ds{file.split(".")[0]}_privateSMOTE_QI{idx}_knn{k}_per{p}_ep{ep}')
+                            put_file_queue(channel, f'ds{file.split(".")[0]}_privateSMOTE_QI{idx}_knn{k}_per{p}_ep{ep}')
+                            
+connection.close()

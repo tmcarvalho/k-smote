b53924668799750db165d46bc51f76b22245ebfa
tmcarvalho <taniammc.94@gmail.com>
2023-06-06 10:08:58 -0400

    add synthpop

--- code/result_performance.py
+++ code/result_performance.py
@@ -18,56 +18,56 @@ def percentage_difference_org(baseline_folder, candidate_folder, technique):
 
     for baseline in baseline_result_files:
         baseline_result_cv_train = pd.read_csv(f'{baseline_folder}/validation/{baseline}')
-        baseline_result_cv = pd.read_csv(f'{baseline_folder}/test/{baseline}')
-        
+        baseline_result_test = pd.read_csv(f'{baseline_folder}/test/{baseline}')
+
         # guaranteeing that we have the best model in the grid search instead of 3 models
         best_baseline_cv = baseline_result_cv_train.loc[baseline_result_cv_train['rank_test_roc_auc_curve'] == 1,:].reset_index(drop=True)
-        baseline_result_cv_best = baseline_result_cv.loc[baseline_result_cv.model == best_baseline_cv.model[0],:]
-        
+        baseline_result_best = baseline_result_test.loc[baseline_result_test.model == best_baseline_cv.model[0],:]
+
         b = list(map(int, re.findall(r'\d+', baseline.split('.')[0])))[0]
         transf_files = [fl for fl in candidate_result_files if list(map(int, re.findall(r'\d+', fl.split('_')[0])))[0] == b]
 
         for transf_file in transf_files:
             candidate_result_cv_train = pd.read_csv(f'{candidate_folder}/validation/{transf_file}')
-            candidate_result_cv = pd.read_csv(f'{candidate_folder}/test/{transf_file}')
+            candidate_result_test = pd.read_csv(f'{candidate_folder}/test/{transf_file}')
             
             # calculate the percentage difference
             # 100 * (Sc - Sb) / Sb
-        
             if ('privateSMOTE' in technique) or (technique == 'deep_learning') or (technique == 'PPT'):
                 best_candidate_cv = candidate_result_cv_train.loc[candidate_result_cv_train['rank_test_roc_auc_curve'] == 1,:]
-                candidate_result_cv_best = candidate_result_cv.iloc[best_candidate_cv.index,:]
-                candidate_result_cv_best['test_roc_auc_perdif'] = 100 * (candidate_result_cv_best['test_roc_auc'].values[0] - baseline_result_cv_best['test_roc_auc'].values[0]) / baseline_result_cv_best['test_roc_auc'].values[0]
+                candidate_result_best = candidate_result_test.iloc[best_candidate_cv.index,:]
+                candidate_result_best['test_roc_auc_perdif'] = 100 * (candidate_result_best['test_roc_auc'].values[0] - baseline_result_best['test_roc_auc'].values[0]) / baseline_result_best['test_roc_auc'].values[0]
 
             else:
                 best_candidate_cv = candidate_result_cv_train.loc[candidate_result_cv_train['rank_test_roc_auc_curve'] == 1,:].reset_index(drop=True)
-                candidate_result_cv_best = candidate_result_cv.loc[candidate_result_cv.model == best_candidate_cv.model[0],:]
-                candidate_result_cv_best['test_roc_auc_perdif'] = 100 * (candidate_result_cv_best['test_roc_auc'].values[0] - baseline_result_cv_best['test_roc_auc'].values[0]) / baseline_result_cv_best['test_roc_auc'].values[0]
+                candidate_result_best = candidate_result_test.loc[candidate_result_test.model == best_candidate_cv.model[0],:]
+                candidate_result_best['test_roc_auc_perdif'] = 100 * (candidate_result_best['test_roc_auc'].values[0] - baseline_result_best['test_roc_auc'].values[0]) / baseline_result_best['test_roc_auc'].values[0]
             # candidate_result_cv['test_fscore_perdif'] = 100 * (candidate_result_cv['test_f1_weighted'] - baseline_result_cv['test_f1_weighted']) / baseline_result_cv['test_f1_weighted']
             # candidate_result_cv['test_gmean_perdif'] = 100 * (candidate_result_cv['test_gmean'] - baseline_result_cv['test_gmean']) / baseline_result_cv['test_gmean']
 
-            oracle = candidate_result_cv.loc[candidate_result_cv['test_roc_auc'].idxmax(),'test_roc_auc']
-            candidate_result_cv_best['test_roc_auc_perdif_oracle'] = 100 * (candidate_result_cv_best['test_roc_auc'] - oracle) / oracle
+            oracle_candidate = candidate_result_test.loc[candidate_result_test['test_roc_auc'].idxmax(),'test_roc_auc']
+            candidate_result_best['test_roc_auc_oracle'] = oracle_candidate
+            candidate_result_best['test_roc_auc_candidate'] = candidate_result_best['test_roc_auc'].values[0]
 
             # get technique
             if technique=='resampling':
-                candidate_result_cv_best.loc[:, 'technique'] = transf_file.split('_')[1].title()
+                candidate_result_best.loc[:, 'technique'] = transf_file.split('_')[1].title()
             elif technique=='deep_learning':
-                candidate_result_cv_best.loc[:, 'technique'] = transf_file.split('_')[1]
+                candidate_result_best.loc[:, 'technique'] = transf_file.split('_')[1]
             else:
-                candidate_result_cv_best.loc[:, 'technique'] = technique
+                candidate_result_best.loc[:, 'technique'] = technique
                 
             
             # get dataset number
-            candidate_result_cv_best['ds'] = transf_file.split('_')[0]
-            candidate_result_cv_best['ds_complete'] = transf_file
+            candidate_result_best['ds'] = transf_file.split('_')[0]
+            candidate_result_best['ds_complete'] = transf_file
 
             # concat each test result
             if c == 0:
-                df_concat_cv = candidate_result_cv_best
+                df_concat_cv = candidate_result_best
                 c += 1
             else:     
-                df_concat_cv = pd.concat([df_concat_cv, candidate_result_cv_best])
+                df_concat_cv = pd.concat([df_concat_cv, candidate_result_best])
 
     return df_concat_cv    
 
@@ -103,13 +103,18 @@ baseorg_privatesmote_laplace = percentage_difference_org(orig_folder, privatesmo
 privatesmote_force_laplace_folder = '../output/modeling/PrivateSMOTE_force_laplace/'
 baseorg_privatesmote_force_laplace = percentage_difference_org(orig_folder, privatesmote_force_laplace_folder, 'privateSMOTE_force_laplace')
 
-# %% DPART
+# %% DPART independent
 dpart_folder = '../output/modeling/dpart_independent/'
 baseorg_dpart = percentage_difference_org(orig_folder, dpart_folder, 'dpart_independent')
 
+# %% DPART synthpop
+dpartsynt_folder = '../output/modeling/dpart_synthpop/'
+baseorg_dpart_synt = percentage_difference_org(orig_folder, dpartsynt_folder, 'dpart_synthpop')
+
 # %% concat all data sets
 results_baseorg_cv = pd.concat([baseorg_ppt, baseorg_resampling, baseorg_deeplearn, baseorg_privatesmote_scratch,
-                                baseorg_privatesmote_force, baseorg_privatesmote_laplace, baseorg_privatesmote_force_laplace, baseorg_dpart])
+                                baseorg_privatesmote_force, baseorg_privatesmote_laplace, baseorg_privatesmote_force_laplace,
+                                baseorg_dpart, baseorg_dpart_synt])
 # %%
 results_baseorg_cv = results_baseorg_cv.reset_index(drop=True)
 # %%
@@ -122,7 +127,8 @@ results_baseorg_cv.loc[results_baseorg_cv['technique']=='privateSMOTE', 'techniq
 results_baseorg_cv.loc[results_baseorg_cv['technique']=='privateSMOTE_force', 'technique'] = 'PrivateSMOTE Force'
 results_baseorg_cv.loc[results_baseorg_cv['technique']=='privateSMOTE_laplace', 'technique'] = r'$\epsilon$-PrivateSMOTE'
 results_baseorg_cv.loc[results_baseorg_cv['technique']=='privateSMOTE_force_laplace', 'technique'] =  r'$\epsilon$-PrivateSMOTE Force'
-results_baseorg_cv.loc[results_baseorg_cv['technique']=='dpart_independent', 'technique'] =  'DPART'
+results_baseorg_cv.loc[results_baseorg_cv['technique']=='dpart_independent', 'technique'] = 'Independent'
+results_baseorg_cv.loc[results_baseorg_cv['technique']=='dpart_synthpop', 'technique'] =  'Synthpop'
 
 # %%
 # results_baseorg_cv.to_csv('../output/test_cv_roc_auc_newprivatesmote.csv', index=False)
@@ -138,7 +144,7 @@ PROPS = {
     'whiskerprops':{'color':'black'},
     'capprops':{'color':'black'}
 }
-order = ['PPT', 'RUS', 'SMOTE', 'BorderlineSMOTE', 'Copula GAN', 'TVAE', 'CTGAN', 'DPART', 'PrivateSMOTE', 'PrivateSMOTE Force', r'$\epsilon$-PrivateSMOTE', r'$\epsilon$-PrivateSMOTE Force']
+order = ['PPT', 'RUS', 'SMOTE', 'BorderlineSMOTE', 'Copula GAN', 'TVAE', 'CTGAN', 'Independent', 'Synthpop', 'PrivateSMOTE', 'PrivateSMOTE Force', r'$\epsilon$-PrivateSMOTE', r'$\epsilon$-PrivateSMOTE Force']
 # %%
 sns.set_style("darkgrid")
 plt.figure(figsize=(12,8))

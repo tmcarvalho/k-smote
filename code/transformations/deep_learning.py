18056527b2144fe6000f186ef2befd8d2e546d60
tmcarvalho <taniammc.94@gmail.com>
2024-01-04 13:54:45 +0000

    use file parameters and remove unused concat

--- code/transformations/deep_learning.py
+++ code/transformations/deep_learning.py
@@ -14,25 +14,17 @@ args = parser.parse_args()
 
 
 def keep_numbers(data):
-    """fix data types according to the data"""
-    data_types = data.copy()
+    """Fix data types according to the data"""
     for col in data.columns:
-        # transform numerical strings to digits
+        # Transform numerical strings to digits
         if isinstance(data[col].iloc[0], str) and data[col].iloc[0].isdigit():
             data[col] = data[col].astype(float)
-        # remove trailing zeros
+        # Remove trailing zeros
         if isinstance(data[col].iloc[0], (int, float)):
             if int(data[col].iloc[0]) == float(data[col].iloc[0]):
                 data[col] = data[col].astype(int)
-            else: data[col] = data_types[col].astype(float)
-    return data, data_types
+    return data
 
-# %%
-epochs=[100, 200]
-batch_size=[50, 100]
-embedding_dim=[32, 64] 
-
-# %%
 
 def synth(msg):
     """Synthesize data using a deep learning model
@@ -45,51 +37,49 @@ def synth(msg):
     """
     print(msg)
     output_interpolation_folder = 'output/oversampled/deep_learning'
-    if msg.split('_')[0] not in ['ds100', 'ds43']:
-        f = list(map(int, re.findall(r'\d+', msg.split('_')[0])))
-        print(str(f[0]))
-        data = pd.read_csv(f'original/{str(f[0])}.csv')
+    f = list(map(int, re.findall(r'\d+', msg.split('_')[0])))
+    print(str(f[0]))
+    data = pd.read_csv(f'original/{str(f[0])}.csv')
+    print(data.shape)
+    # get 80% of data to synthesise
+    indexes = np.load('indexes.npy', allow_pickle=True).item()
+    indexes = pd.DataFrame.from_dict(indexes)
 
-        # get 80% of data to synthesise
-        indexes = np.load('indexes.npy', allow_pickle=True).item()
-        indexes = pd.DataFrame.from_dict(indexes)
+    index = indexes.loc[indexes['ds']==str(f[0]), 'indexes'].values[0]
+    data_idx = list(set(list(data.index)) - set(index))
+    data = data.iloc[data_idx, :]
+    print(data.shape)
+    # encode string with numbers to numeric and remove trailing zeros
+    data = keep_numbers(data)
 
-        index = indexes.loc[indexes['ds']==str(f[0]), 'indexes'].values[0]
-        data_idx = list(set(list(data.index)) - set(index))
-        data = data.iloc[data_idx, :]
+    # transform target to string because integer targets are not well synthesised
+    data[data.columns[-1]] = data[data.columns[-1]].astype(str)
+    
+    technique = msg.split('_')[1]
+    print(technique)
+    ep = list(map(int, re.findall(r'\d+', msg.split('_')[2])))[0]
+    bs = list(map(int, re.findall(r'\d+', msg.split('_')[3])))[0]
+    
+    metadata = SingleTableMetadata()
+    metadata.detect_from_dataframe(data=data)
 
-        # encode string with numbers to numeric and remove trailing zeros
-        data, _ = keep_numbers(data)
+    print("epochs: ", ep)
+    print("batch_size: ", bs)
+    if technique=='CTGAN':
+        model = CTGANSynthesizer(metadata, epochs=ep, batch_size=bs)
+    elif technique=='TVAE':
+        model = TVAESynthesizer(metadata, epochs=ep, batch_size=bs)
+    else:
+        model = CopulaGANSynthesizer(metadata, epochs=ep, batch_size=bs)
 
-        # transform target to string because integer targets are not well synthesised
-        data[data.columns[-1]] = data[data.columns[-1]].astype(str)
+    # Generate synthetic data 
+    model.fit(data)
+    # Generate synthetic data
+    new_data = model.sample(num_rows=len(data))             
+
+    # Save the synthetic data
+    new_data.to_csv(
+        f'{output_interpolation_folder}{sep}{msg}.csv',
+        index=False)
         
-        technique = msg.split('_')[1]
-        print(technique)
-        ep = list(map(int, re.findall(r'\d+', msg.split('_')[3])))[0]
-        bs = list(map(int, re.findall(r'\d+', msg.split('_')[4])))[0]
-        
-        metadata = SingleTableMetadata()
-        metadata.detect_from_dataframe(data=data)
-
-        print("epochs: ", epochs[0])
-        print("batch_size: ", batch_size[0])
-        if technique=='CTGAN':
-            model = CTGANSynthesizer(metadata, epochs=epochs[0], batch_size=batch_size[0], verbose=True)
-        elif technique=='TVAE':
-            model = TVAESynthesizer(metadata, epochs=epochs[0], batch_size=batch_size[0])
-        else:
-            model = CopulaGANSynthesizer(metadata, epochs=epochs[0], batch_size=batch_size[0], verbose=True)
-
-        # Generate synthetic data 
-        model.fit(data)
-        # Generate synthetic data
-        new_data = model.sample(num_rows=len(data))             
-        new_data_ = pd.concat([new_data, data])
-
-        # Save the synthetic data
-        new_data_.to_csv(
-            f'{output_interpolation_folder}{sep}{msg}.csv',
-            index=False)
-        
-synth(args.input)
+synth(args.input_file)

23b638679756d6eb99ea5993e33043874f5a7195
tmcarvalho <taniammc.94@gmail.com>
2024-01-16 16:38:41 +0000

    update all results

--- code/results_anonymeter.py
+++ code/results_anonymeter.py
@@ -29,7 +29,7 @@ risk_deeplearning= concat_each_file('../output/anonymeter/deep_learning')
 risk_city = concat_each_file('../output/anonymeter/city')
 # %%
 risk_privateSMOTE = concat_each_file('../output/anonymeter/PrivateSMOTE')
-# TODO: run all QIs
+
 # %%
 risk_ppt['technique'] = 'PPT'
 risk_resampling['technique'] = risk_resampling['ds_complete'].apply(lambda x: x.split('_')[1].title())
@@ -52,7 +52,7 @@ results.loc[results['technique']=='CopulaGAN', 'technique'] = 'Copula GAN'
 # results.to_csv('../output_analysis/anonymeter.csv', index=False)
 
 # %%
-results = pd.read_csv('../output_analysis/anonymeter.csv')
+# results = pd.read_csv('../output_analysis/anonymeter.csv')
 # %%
 order = ['PPT', 'RUS', 'SMOTE', 'BorderlineSMOTE', 'Copula GAN', 'TVAE', 'CTGAN', 'DPGAN', 'PATEGAN', r'$\epsilon$-PrivateSMOTE']
 # %%
@@ -68,8 +68,6 @@ plt.xticks(rotation=45)
 plt.xlabel("")
 plt.ylabel("Re-identification Risk")
 plt.show()
-# figure = ax.get_figure()
-# figure.savefig(f'{os.path.dirname(os.getcwd())}/output/plots/anonymeter_k5.pdf', bbox_inches='tight')
 
 # %%  BETTER IN PRIVACY
 results_risk_min = results.loc[results.groupby(['ds', 'technique'])['value'].idxmin()].reset_index(drop=True)
@@ -85,8 +83,6 @@ plt.xticks(rotation=45)
 plt.xlabel("")
 plt.ylabel("Re-identification Risk")
 plt.show()
-# figure = ax.get_figure()
-# figure.savefig(f'{os.path.dirname(os.getcwd())}/output/plots/anonymeter_k5.pdf', bbox_inches='tight')
 
 
 # %%
@@ -103,6 +99,20 @@ PROPS = {
     'whiskerprops':{'color':'black'},
     'capprops':{'color':'black'}
 }
+
+ep_order = ['0.1', '0.5', '1.0', '5.0', '10.0']
+sns.set_style("darkgrid")
+plt.figure(figsize=(8,7))
+ax = sns.boxplot(data=privsmote, x='epsilon', y='value', order=ep_order, **PROPS)
+sns.set(font_scale=1.5)
+plt.xticks(rotation=45)
+plt.xlabel("")
+plt.ylabel("Re-identification Risk")
+# ax.set_xlim(0,1.02)
+plt.show()
+# plt.savefig(f'{os.path.dirname(os.getcwd())}/plots/privateSMOTE_epsilons_risk.pdf', bbox_inches='tight')
+
+# %%
 privsmote_max = privsmote.loc[privsmote.groupby(['ds', 'epsilon'])['value'].idxmin()]
 
 ep_order = ['0.1', '0.5', '1.0', '5.0', '10.0']
@@ -115,7 +125,4 @@ plt.xlabel("")
 plt.ylabel("Re-identification Risk")
 # ax.set_xlim(0,1.02)
 plt.show()
-# figure = ax.get_figure()
-# figure.savefig(f'{os.path.dirname(os.getcwd())}/plots/privateSMOTE_epsilons_risk.pdf', bbox_inches='tight')
-
 # %%

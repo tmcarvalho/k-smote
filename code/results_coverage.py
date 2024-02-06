2a38c85d919a2e66a3a986d82f71dd6145dc1851
tmcarvalho <taniammc.94@gmail.com>
2024-02-06 15:50:54 +0000

    change colors

--- code/results_coverage.py
+++ code/results_coverage.py
@@ -126,7 +126,7 @@ sns.boxplot(ax=axes[2], data=cov_performance_best,
 sns.boxplot(ax=axes[3], data=cov_performance_best,
     x='Correlation', hue=cov_performance_best['technique_x'], hue_order=order)
 sns.set(font_scale=2.4)
-sns.set_palette("Paired")
+sns.set_palette("flare")
 axes[0].set_xlim(0,1.02)
 axes[1].set_xlim(0,1.02)
 axes[2].set_xlim(0,1.02)
@@ -136,7 +136,7 @@ axes[1].get_legend().set_visible(False)
 axes[3].get_legend().set_visible(False)
 # plt.subplots_adjust(wspace = 0.1)
 sns.move_legend(axes[2], title='Transformation Techniques', bbox_to_anchor=(-0.1,1.3), loc='upper center', borderaxespad=0., ncol=6, frameon=False)
-# plt.savefig(f'{os.path.dirname(os.getcwd())}/plots/utility_best_techniques.pdf', bbox_inches='tight')
+plt.savefig(f'{os.path.dirname(os.getcwd())}/plots/utility_best_techniques.pdf', bbox_inches='tight')
 
 # %%
 privsmote = mean_cols.loc[mean_cols.technique.str.contains('PrivateSMOTE')].reset_index(drop=True)
@@ -212,7 +212,7 @@ sns.boxplot(ax=axes[2], data=privsmote,
 sns.boxplot(ax=axes[3], data=privsmote,
     x='Correlation', hue=privsmote[r'$\epsilon$'], hue_order=hue_order)
 sns.set(font_scale=2.25)
-sns.set_palette("Set2")
+sns.set_palette("viridis")
 axes[0].set_xlim(0.45,1.02)
 axes[1].set_xlim(0.45,1.02)
 axes[2].set_xlim(0.45,1.02)
@@ -225,21 +225,27 @@ sns.move_legend(axes[2], bbox_to_anchor=(-0.1,1.3), loc='upper center', borderax
 
 # %% DS16
 privsmote_ds16 = privsmote.loc[privsmote.dsn=='ds16']
-plt.figure(figsize=(6,4))
 sns.set_style("darkgrid")
-ax4 = sns.boxplot(x=privsmote_ds16["Range Coverage"], hue=privsmote_ds16[r'$\epsilon$'], hue_order=hue_order)
-sns.move_legend(ax4, bbox_to_anchor=(1,0.5), loc='center left', borderaxespad=0., frameon=False)
-sns.set(font_scale=1)
-sns.set_palette("Paired")
-# plt.savefig(f'{os.path.dirname(os.getcwd())}/plots/range_coverage_privatesmote_ds16.pdf', bbox_inches='tight')
-# %% 
-plt.figure(figsize=(6,4))
-sns.set_style("darkgrid")
-ax4 = sns.boxplot(x=privsmote_ds16["Statistic Similarity (Mean)"], hue=privsmote_ds16[r'$\epsilon$'], hue_order=hue_order)
-sns.move_legend(ax4, bbox_to_anchor=(1,0.5), loc='center left', borderaxespad=0., frameon=False)
-sns.set(font_scale=1)
-sns.set_palette("Paired")
-# plt.savefig(f'{os.path.dirname(os.getcwd())}/plots/statistic_similarity_privatesmote_ds16.pdf', bbox_inches='tight')
+fig, axes = plt.subplots(1, 4, figsize=(23,6))
+sns.boxplot(ax=axes[0], data=privsmote_ds16,
+    x='Range Coverage', hue=privsmote_ds16[r'$\epsilon$'], hue_order=hue_order)
+sns.boxplot(ax=axes[1], data=privsmote_ds16,
+    x='Boundary Adherence', hue=privsmote_ds16[r'$\epsilon$'], hue_order=hue_order)
+sns.boxplot(ax=axes[2], data=privsmote_ds16,
+    x='Statistic Similarity (Mean)', hue=privsmote_ds16[r'$\epsilon$'], hue_order=hue_order)
+sns.boxplot(ax=axes[3], data=privsmote_ds16,
+    x='Correlation', hue=privsmote_ds16[r'$\epsilon$'], hue_order=hue_order)
+sns.set(font_scale=2.25)
+sns.set_palette("crest")
+axes[0].set_xlim(0.8,1.02)
+axes[1].set_xlim(0.8,1.02)
+axes[2].set_xlim(0.8,1.02)
+axes[3].set_xlim(0.8,1.02)
+axes[0].get_legend().set_visible(False)
+axes[1].get_legend().set_visible(False)
+axes[3].get_legend().set_visible(False)
+sns.move_legend(axes[2], bbox_to_anchor=(-0.1,1.3), loc='upper center', borderaxespad=0., ncol=6, frameon=False)
+# plt.savefig(f'{os.path.dirname(os.getcwd())}/plots/utility_epsilons_ds16.pdf', bbox_inches='tight')
 
 # %% Particular analysis
 mean_cols.loc[(mean_cols["Range Coverage"]<0.5) & (mean_cols["technique"]=='SDV')]
